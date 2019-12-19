2a353482c1854fddd6d3f2eabcab79d9cac9037e
Keeran Sivaneri <KSivaneri@ucvts.tec.nj.us>
2019-12-19 10:41:08 -0500

    did some stuff

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-// hello
+let items = [];
+
+window.onload = function() {
+    document.getElementById("test").onclick = createItem;
+}
+
+const createItem = function() {
+    let newItem = window.prompt();
+    newItem = newItem.trim();
+
+    items.push(newItem);
+
+    window.alert(items);
+}
