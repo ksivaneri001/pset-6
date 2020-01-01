afaef4bd5972009f83f866b0b1bd18d1644aa412
Keniran <naani.navis@gmail.com>
2019-12-31 20:29:24 -0500

    user can now add items

--- js/app.js
+++ js/app.js
@@ -6,8 +6,40 @@ window.onload = function() {
 
 const createItem = function() {
     let input = document.getElementById("enter-input").value;
+    if (input === "") {}
+    else {
+        let object = {
+            task: input,
+            completed: false,
+            prioritized: false,
+        }
 
-    window.alert(input);
+        items.push(object);
 
+        let id = items.indexOf(object);
+
+        let newRow = document.createElement("tr");
+        newRow.setAttribute("id", "row-" + id);
+        document.getElementById("task-table").append(newRow);
+
+        let priorityButton = document.createElement("td");
+        priorityButton.setAttribute("id", "priority-button-" + id);
+        priorityButton.innerHTML = "!";
+        document.getElementById("row-" + id).append(priorityButton);
+
+        let newTask = document.createElement("td");
+        newTask.innerHTML = input;
+        document.getElementById("row-" + id).append(newTask);
+
+        let completedButton = document.createElement("td");
+        completedButton.setAttribute("id", "completed-button-" + id);
+        completedButton.innerHTML = "O";
+        document.getElementById("row-" + id).append(completedButton);
+
+        let removeButton = document.createElement("td");
+        removeButton.setAttribute("id", "remove-button-" + id);
+        removeButton.innerHTML = "X";
+        document.getElementById("row-" + id).append(removeButton);
+    }
     document.getElementById("enter-input").value = "";
 }
