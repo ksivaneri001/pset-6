b4a6f0e5fde85dffe4e23f0883c4363051b9c30b
Keniran <naani.navis@gmail.com>
2020-01-01 22:18:41 -0500

    Changed everything to classes, but commented out old code just in case it is needed

--- js/app.js
+++ js/app.js
@@ -1,30 +1,33 @@
 let items = [];
+
+let elements = document.getElementsByClassName("row");
+let priorityButtons = document.getElementsByClassName("priority-button");
+let completedButtons = document.getElementsByClassName("completed-button");
+let removeButtons = document.getElementsByClassName("remove-button");
+
 let itemRemoved;
 
 window.onload = function() {
     document.getElementById("task-table").onclick = removeButtonCheck;
 
     document.getElementById("submit-input").onclick = createItem;
-    document.getElementById("test-button").onclick = test;
-
-    // document.getElementById("test-2").onclick = test2;
 }
 
-const test2 = function() {
-    alert("test-2");
+const test = function() {
+    alert("test");
 }
 
-const updateId = function() {
-    for (let i = 0; i < items.length; i++) {
-        items[i].htmlRow.setAttribute("id", "row-" + i);
-        items[i].htmlPriorityButton.setAttribute("id", "priority-button-" + i);
-        items[i].htmlCompletedButton.setAttribute("id", "completed-button-" + i);
-        items[i].htmlRemoveButton.setAttribute("id", "remove-button-" + i);
-    }
-}
+// const updateId = function() {
+//     for (let i = 0; i < items.length; i++) {
+//         items[i].htmlRow.setAttribute("id", "row-" + i);
+//         items[i].htmlPriorityButton.setAttribute("id", "priority-button-" + i);
+//         items[i].htmlCompletedButton.setAttribute("id", "completed-button-" + i);
+//         items[i].htmlRemoveButton.setAttribute("id", "remove-button-" + i);
+//     }
+// }
 
 function removeItem(y) {
-    const elementToRemove = document.getElementById("row-" + y);
+    const elementToRemove = elements[y];
     elementToRemove.remove();
     items.splice(y, 1);
 
@@ -36,14 +39,14 @@ function removeItem(y) {
 const removeButtonCheck = function() {
     itemRemoved = false;
 
-    for (let i = 0; i < document.getElementsByClassName("remove-button").length; i++) {
-        document.getElementsByClassName("remove-button")[i].onclick = test;
+    for (let i = 0; i < removeButtons.length; i++) {
+        removeButtons[i].onclick = test;
+
         if (itemRemoved) {
             break;
         }
     }
 }
-
 const createItem = function() {
     let input = document.getElementById("enter-input").value;
     if (input === "") {}
@@ -64,40 +67,37 @@ const createItem = function() {
         let x = items.indexOf(object);
 
         items[x].htmlRow = document.createElement("tr");
-        items[x].htmlRow.setAttribute("id", "row-" + x);
+        // items[x].htmlRow.setAttribute("id", "row-" + x);
+        items[x].htmlRow.setAttribute("class", "row");
         document.getElementById("task-table").append(items[x].htmlRow);
 
         items[x].htmlPriorityButton = document.createElement("td");
-        items[x].htmlPriorityButton.setAttribute("id", "priority-button-" + x);
+        // items[x].htmlPriorityButton.setAttribute("id", "priority-button-" + x);
         items[x].htmlPriorityButton.setAttribute("class", "priority-button");
         items[x].htmlPriorityButton.innerHTML = "!";
-        document.getElementById("row-" + x).append(items[x].htmlPriorityButton);
+        // document.getElementById("row-" + x).append(items[x].htmlPriorityButton);
+        elements[x].append(items[x].htmlPriorityButton);
 
         items[x].htmlText = document.createElement("td");
         items[x].htmlText.innerHTML = items[x].task;
-        document.getElementById("row-" + x).append(items[x].htmlText);
+        // document.getElementById("row-" + x).append(items[x].htmlText);
+        elements[x].append(items[x].htmlText);
 
         items[x].htmlCompletedButton = document.createElement("td");
-        items[x].htmlCompletedButton.setAttribute("id", "completed-button-" + x);
+        // items[x].htmlCompletedButton.setAttribute("id", "completed-button-" + x);
         items[x].htmlCompletedButton.setAttribute("class", "completed-button");
         items[x].htmlCompletedButton.innerHTML = "O";
-        document.getElementById("row-" + x).append(items[x].htmlCompletedButton);
+        // document.getElementById("row-" + x).append(items[x].htmlCompletedButton);
+        elements[x].append(items[x].htmlCompletedButton);
 
         items[x].htmlRemoveButton = document.createElement("td");
-        items[x].htmlRemoveButton.setAttribute("id", "remove-button-" + x);
+        // items[x].htmlRemoveButton.setAttribute("id", "remove-button-" + x);
         items[x].htmlRemoveButton.setAttribute("class", "remove-button");
         items[x].htmlRemoveButton.innerHTML = "X";
-        document.getElementById("row-" + x).append(items[x].htmlRemoveButton);
+        // document.getElementById("row-" + x).append(items[x].htmlRemoveButton);
+        elements[x].append(items[x].htmlRemoveButton);
 
-        updateId();
+        // updateId();
     }
     document.getElementById("enter-input").value = "";
-}
-
-const test = function() {
-    const element = document.getElementById("row-0");
-    element.remove();
-    items.shift();
-
-    updateId();
 }
